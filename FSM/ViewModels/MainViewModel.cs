d6cc3eb4e16e4bd353e8ca671d35dc3f70558e0b
Phantomzzz7 <raresanton14@gmail.com>
2026-09-13 15:13:08 +0300

    Added course creation with RelayCommand and MVVM binding

--- FSM/ViewModels/MainViewModel.cs
+++ FSM/ViewModels/MainViewModel.cs
@@ -4,6 +4,7 @@ using FSM.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.ObjectModel;
 using System.Linq;
+using CommunityToolkit.Mvvm.Input;
 
 namespace FSM.ViewModels
 {
@@ -13,6 +14,15 @@ namespace FSM.ViewModels
         
         [ObservableProperty]
         private ObservableCollection<Course> courses=new();
+
+        [ObservableProperty]
+        private string newCourseName = string.Empty;
+
+        [ObservableProperty]
+        private string newCourseProfessor = string.Empty;
+
+        [ObservableProperty]
+        private string newCourseRoom = string.Empty;
 
         public MainViewModel()
         {
@@ -24,6 +34,27 @@ namespace FSM.ViewModels
         private void LoadCourses()
         {
             Courses = new ObservableCollection<Course>(db.Courses.ToList());
+        }
+
+        [RelayCommand]
+        private void AddCourse()
+        {
+            if (string.IsNullOrWhiteSpace(newCourseName))
+                return;
+            var course = new Course
+            {
+                Name = newCourseName,
+                Professor = newCourseProfessor,
+                Room = newCourseRoom
+            };
+            db.Courses.Add(course);
+            db.SaveChanges();
+
+            newCourseName = string.Empty;
+            newCourseProfessor = string.Empty;
+            newCourseRoom = string.Empty;
+
+            LoadCourses();
         }
     }
 }
